61a674e6d1bce1c8b3bd7d36d507f463073bc4b5
Andrew Johnston <ajohnston@interac.ca>
2022-10-06 17:42:35 -0400

    Updated build to generate both DOC and PDF

--- build/build.sh
+++ build/build.sh
@@ -2,5 +2,5 @@
 
 BASEDIR=$(dirname "$0")
 
-pandoc --defaults="$BASEDIR"/pandoc_defaults.yaml
-
+pandoc --defaults="$BASEDIR"/pandoc_defaults.yaml --output "$BASEDIR"/ts-115-working-draft.docx --to docx
+pandoc --defaults="$BASEDIR"/pandoc_defaults.yaml --output "$BASEDIR"/ts-115-working-draft.pdf --to pdf
