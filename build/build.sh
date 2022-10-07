65f43a0a46c08cd224cf5c893597b669a054f026
trbouma@gmail.com <trbouma@gmail.com>
2022-10-07 18:23:35 -0400

    chained pdf to docx output

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,5 @@
 BASEDIR=$(dirname "$0")
 
 pandoc --defaults="$BASEDIR"/pandoc_defaults.yaml --output "$BASEDIR"/ts-115-working-draft.docx --to docx --reference-doc="$BASEDIR"/CIOSC-reference.docx
-pandoc --defaults="$BASEDIR"/pandoc_defaults.yaml --output "$BASEDIR"/ts-115-working-draft.pdf --to pdf
+pandoc "$BASEDIR"/ts-115-working-draft.docx --output "$BASEDIR"/ts-115-working-draft.pdf
+# pandoc --defaults="$BASEDIR"/pandoc_defaults.yaml --output "$BASEDIR"/ts-115-working-draft.pdf --to pdf
