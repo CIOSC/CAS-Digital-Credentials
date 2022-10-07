6643c3ef5374a1fbbcd81348946190c198c53058
Andrew Johnston <ajohnston@interac.ca>
2022-10-07 16:15:34 -0400

    Added Word template/styleguide

--- build/build.sh
+++ build/build.sh
@@ -2,5 +2,5 @@
 
 BASEDIR=$(dirname "$0")
 
-pandoc --defaults="$BASEDIR"/pandoc_defaults.yaml --output "$BASEDIR"/ts-115-working-draft.docx --to docx
+pandoc --defaults="$BASEDIR"/pandoc_defaults.yaml --output "$BASEDIR"/ts-115-working-draft.docx --to docx --reference-doc="$BASEDIR"/CIOSC-reference.docx
 pandoc --defaults="$BASEDIR"/pandoc_defaults.yaml --output "$BASEDIR"/ts-115-working-draft.pdf --to pdf
